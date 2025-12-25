adca44e06a6ba334c443e9dded7820777bda5248
Naman <namanagarwal686@gmail.com>
2025-12-25 18:55:51 +0530

    Set up Express server with CORS support and update package dependencies

--- MongoDB-1/index.js
+++ MongoDB-1/index.js
@@ -1,3 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from './app.js'
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+    .then(() => {
+
+        app.on("error", (error) => {
+            console.log("Express App Error: ", error);
+            throw error;
+        });
+
+        app.listen(8000, () => {
+            console.log(`Server is running on port 8000`);
+        })
+    })
+
+    .catch((error) => {
+        console.log("MongoDB Connection Failed: ", error);
+    })
